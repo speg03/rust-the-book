a7d61b7994709adcd3fed5bb93d4eade8de67db9
Takahiro Yano <speg03@gmail.com>
2023-04-19 23:06:56 +0900

    乱数を生成する

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");          // 数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
 
     println!("Please input your guess.");   // ほら、予想を入力してね
 
